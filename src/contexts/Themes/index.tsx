2b8aa0b35af1efe18e22757c6a4b30f0b86175ac
EugeneWay <relax211@gmail.com>
2023-09-19 19:42:44 +0200

    fix icon & theme

--- src/contexts/Themes/index.tsx
+++ src/contexts/Themes/index.tsx
@@ -16,7 +16,7 @@ export const ThemesProvider = ({ children }: { children: React.ReactNode }) => {
   if (!['light', 'dark'].includes(localThemeRaw)) {
     const systemTheme =
       window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
+        window.matchMedia('(prefers-color-scheme: dark)').matches
         ? 'dark'
         : 'light';
 
@@ -52,7 +52,7 @@ export const ThemesProvider = ({ children }: { children: React.ReactNode }) => {
     <ThemeContext.Provider
       value={{
         toggleTheme,
-        mode: themeRef.current,
+        mode: initialTheme,
       }}
     >
       {children}
